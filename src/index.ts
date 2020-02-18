c3735be157d304fe8062ffa6f294b7f42424e9ac
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2020-02-18 17:11:00 +0200

    Add cors handle

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { config } from 'dotenv';
+import cors from 'cors';
 
 //Initialize express
 const app = express();
@@ -11,11 +12,20 @@ import { MongoHelper } from './utils/db';
 const baseUrl = '/api/kino/stats'
 app.use(express.json());
 
+const options:cors.CorsOptions = {
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+    credentials: true,
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+    preflightContinue: false
+  };
+
+app.use(cors(options));
+
 //Import routes
 import * as basicRoutes from './routes/basic.routes';
 
 //Use Routes
-app.use(`${baseUrl}`, basicRoutes.router);
+app.use(`${baseUrl}`,cors(), basicRoutes.router);
 
 //Error handling 404
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
