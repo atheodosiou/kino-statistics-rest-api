940cd17295be81debfacb33fcf11c9b73a2f6638
tasos <theodosiout@or-co.gr>
2020-03-25 21:24:41 +0200

    Configure ecosystem.config.js file for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,19 @@
 module.exports = {
-  apps : [{
-    name: 'API',
-    script: 'app.js',
-
-    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }],
-
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
-};
+  apps : [
+      {
+        name: "Kino statistics rest api",
+        script: "npm run build && node dist/index.js",
+        watch: true,
+        env: {
+          "PORT":4242,
+          "NODE_ENV": "development",
+          "CONNECTION_STRING":"mongodb://localhost:27017/kinoResults"
+        },
+        env_production:{
+          "PORT":4242,
+          "NODE_ENV": "production",
+          "CONNECTION_STRING":"mongodb://localhost:27017/kinoResults"
+        }
+      }
+  ]
+}
